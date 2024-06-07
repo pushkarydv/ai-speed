62a818617d5ab1f7430280b01633684ce7d05f9a
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2024-06-08 01:03:10 +0530

    chore: Update Tailwind CSS configuration and dependencies

---
+++ src/app/api/ai/route.js
@@ -0,0 +1,58 @@
+import { createOpenAI } from '@ai-sdk/openai';
+import { OpenAIEmbeddings } from '@langchain/openai';
+import { PineconeStore } from '@langchain/pinecone';
+import { Pinecone } from '@pinecone-database/pinecone';
+import { LangChainAdapter, StreamingTextResponse, streamText } from 'ai';
+
+export const dynamic = 'force-dynamic';
+export const maxDuration = 60;
+export const runtime = 'edge';
+
+export async function POST(req) {
+  const { messages } = await req.json();
+  const prompt = messages[messages.length - 1].content;
+  try {
+    // Initializing all services
+    const pinecone = new Pinecone({
+      apiKey: process.env.PINECONE_API_KEY,
+    });
+    const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX);
+
+    const embeddings = new OpenAIEmbeddings({
+      openAIApiKey: process.env.OPENAI_API_KEY,
+      model: 'text-embedding-3-small',
+    });
+
+    // const model = new ChatGroq({
+    //   apiKey: process.env.GROQ_CLOUD_API_KEY,
+    //   model: 'llama3-8b-8192',
+    //   streaming: true,
+    // });
+
+    const groq = createOpenAI({
+      baseURL: 'https://api.groq.com/openai/v1',
+      apiKey: process.env.GROQ_CLOUD_API_KEY,
+    });
+
+    const vectorStore = new PineconeStore(embeddings, { pineconeIndex });
+
+    const results = await vectorStore.maxMarginalRelevanceSearch(prompt, {
+      k: 5,
+    });
+
+    const documents = results.map((doc) => doc.pageContent).join('\n\n');
+    const newPrompt = `Given the following documents, answer the query: ${prompt}\n\nDocuments:\n${documents}`;
+
+    messages.push({ role: 'system', content: newPrompt });
+
+    const result = await streamText({
+      model: groq('llama3-8b-8192'),
+      messages,
+    });
+
+    return result.toAIStreamResponse();
+  } catch (err) {
+    console.log(err);
+    return Response.error({ message: 'An error occurred!' });
+  }
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,56 @@
+'use client';
+import React, { useState } from 'react';
+import { useChat, useCompletion } from 'ai/react';
+import { marked } from 'marked';
+
 export default function Home() {
-  return <main>Hello!</main>;
+  const { messages, input, handleInputChange, handleSubmit, isLoading } =
+    useChat({
+      api: '/api/ai',
+    });
+  return (
+    <div className='min-h-screen bg-orange-50 p-2'>
+      <form
+        onSubmit={handleSubmit}
+        className={`flex items-center gap-2 w-[90%] md:w-full max-w-2xl p-2 bg-white ring-1 ring-gray-100 rounded-full fixed z-10 left-1/2 -translate-x-1/2 ${
+          messages.length === 0 ? 'bottom-1/2 translate-y-1/2' : 'bottom-4'
+        }`}
+      >
+        <input
+          type='text'
+          disabled={isLoading}
+          value={input}
+          onChange={handleInputChange}
+          name='prompt'
+          placeholder='What&apos;s up buddy'
+          required
+          autoComplete='off'
+          className='w-full rounded-full py-1 px-2 outline-none border-none'
+        />
+        <button
+          type='submit'
+          disabled={isLoading}
+          className='bg-orange-500 text-white rounded-full py-1 px-2 disabled:bg-orange-300'
+        >
+          {isLoading ? 'Generating...' : 'Generate'}
+        </button>
+      </form>
+
+      <div className='prose max-w-4xl mx-auto pt-4 pb-16'>
+        {messages.map((m) => (
+          <div
+            key={m.id}
+            className={`mb-4 sm:max-w-2xl md:max-w-3xl w-fit rounded-xl shadow shadow-orange-100 px-3 py-1 prose-add ${
+              m.role === 'user'
+                ? 'mr-0 ml-auto bg-orange-400 border-t-2 border-t-orange-300 border-b-2 border-b-orange-500 text-white'
+                : 'bg-white'
+            }`}
+            dangerouslySetInnerHTML={{
+              __html: m.content ? marked.parse(m.content) : '',
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
 }
