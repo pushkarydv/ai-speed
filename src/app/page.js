b2476ca2302b6f062ae0a26ea692fa07ca446664
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2024-06-08 11:32:17 +0530

    chore: Remove llama-3 mode and add fast mode indicator

--- src/app/page.js
+++ src/app/page.js
@@ -10,20 +10,22 @@ export default function Home() {
     });
   return (
     <div className='min-h-screen bg-orange-50 p-2'>
-      <div className='fixed top-0 left-0 z-10 opacity-60'>llama-3 mode</div>
       <form
         onSubmit={handleSubmit}
         className={`flex items-center gap-2 w-[90%] md:w-full max-w-2xl p-2 bg-white ring-1 ring-gray-100 rounded-full fixed z-10 left-1/2 -translate-x-1/2 ${
           messages.length === 0 ? 'bottom-1/2 translate-y-1/2' : 'bottom-4'
         }`}
       >
+        <div className='fixed -top-6 left-4 z-10 bg-gradient-to-tr from-sky-400 to-blue-500 text-white text-sm px-1 rounded-md'>
+          Fast Mode⏩
+        </div>
         <input
           type='text'
           disabled={isLoading}
           value={input}
           onChange={handleInputChange}
           name='prompt'
-          placeholder='What&apos;s up buddy'
+          placeholder="What's up buddy"
           required
           autoComplete='off'
           className='w-full rounded-full py-1 px-2 outline-none border-none'
